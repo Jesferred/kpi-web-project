50c6c2e8450c390f137bc5a126c4b70dac45f243
kangen <kck.tema@gmail.com>
2024-09-21 13:18:19 +0300

    Add pagesViews tests

--- db.js
+++ db.js
@@ -1,13 +1,42 @@
+import dotenv from 'dotenv'
 import Sequelize from 'sequelize';
+import config from './config/config.js';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
+dotenv.config({ path: '.env.test' }); // Подгружаем файл .env.test
+
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
+
+let sequelize;
+
+if (dbConfig.use_env_variable) {
+    sequelize = new Sequelize(process.env[dbConfig.use_env_variable], {
+        dialect: dbConfig.dialect,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false, // Временное решение, если сертификат не проверяется
+            },
         },
-    },
-});
+        logging: false,
+    });
+} else {
+    sequelize = new Sequelize(
+        dbConfig.database,
+        dbConfig.username,
+        dbConfig.password,
+        {
+            host: dbConfig.host,
+            dialect: dbConfig.dialect,
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false,
+                },
+            },
+        }
+    );
+}
+
 
 export default sequelize;
