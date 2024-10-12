7200b3f8215b8907036315d75af55ed87d5dd2dd
Artem.Koba <artemkoba04@gmail.com>
2024-10-12 17:33:02 +0300

    2nd pop up

--- public/js/popup.js
+++ public/js/popup.js
@@ -6,4 +6,11 @@ $('.close-popup').click(function(){
     $('.popup-bg').fadeOut(600);
 });
 
+$('.settings-btn').click(function(){
+    $('.popup-bg-settings').fadeIn(600);
+});
+
+$('.close-popup-settings').click(function(){
+    $('.popup-bg-settings').fadeOut(600);
+});
 
